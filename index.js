b3bb00ee2f9a2ee794f673dbc88de66f18485b49
lluvia <friasllu@gmail.com>
2022-12-29 18:12:50 -0500

    commit tree a936469a0b917d061c640c778286152ec008a7d9

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
-// Write your solution here!
+const cats = [
+    "Milo", "Otis", "Garfield"
+];
+
+function destructivelyAppendCat(name){
+    cats.push(name);
+}
+destructivelyAppendCat
+
+function destructivelyPrependCat(name){
+    cats.unshift(name);
+}
+destructivelyPrependCat
+
+function destructivelyRemoveLastCat(){
+    cats.pop();
+}
+destructivelyRemoveLastCat
+
+function destructivelyRemoveFirstCat(){
+    cats.shift();
+}
+destructivelyRemoveFirstCat
+
+function appendCat(name){
+    const appendCat = [...cats, name];
+    return appendCat;
+}
+appendCat
+
+function prependCat(name){
+    const prependCat = [name, ...cats];
+    return prependCat;
+}
+prependCat
+
+
+function removeLastCat(){
+   const removeLastCat = cats.slice(0,-1);
+   return removeLastCat;
+}
+removeLastCat
+
+function removeFirstCat(){
+    const removeFirstCat = cats.slice(1);
+    return removeFirstCat;
+}
+removeFirstCat
